dadd27c53ecc01500d820ae90d1b19fae24e15a9
Terrance Danckert <tbarnett2020@gmail.com>
2018-10-25 16:29:44 -0400

    Component Setup

---
+++ src/component/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+import Game from './Game';
+
+class App extends Component {
+  render() {
+    return (
+  <div>
+    <Game />
+  </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/Game.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Stars from './Stars';
+import Button from './Button';
+import Answer from './Answer';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Play Nine</h3>
+        <Stars />
+        <Button />
+        <Answer />
+      </div>
+    )
+  }
+}
+
+export default Game;
